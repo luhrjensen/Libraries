f31c91613f7abadf5876271be5d8f074e2692eb7
ThomasNelson <tom@kleininstruments.com>
2012-11-26 17:18:59 -0800

    Updated XCode Wrapper

--- XCode/NSKClmtr/NSKClmtr/NSKClmtr.h
+++ XCode/NSKClmtr/NSKClmtr/NSKClmtr.h
@@ -18,129 +18,67 @@ public:
     void printFlick(Flicker f);
     NSKClmtr* _NSKClmtr;
 };
-@interface NSKMatrix : NSObject {
-@public
-    double v[3][3];
-}
--(id)initWithMatrix:(matrix)Matrix;
-@end
 @interface NSMeasurement : NSObject {
-@public
-    double x;               //The x in the xyl
-    double y;               //The y in the xyl
-    double l;               //The l in the xyl
-    double bigx;            //The x in the xyz
-    double bigy;            //The y in the xyz
-    double bigz;            //The z in the xyz
-    double bigxraw;         //The xraw in the xyz
-    double bigyraw;         //The yraw in the xyz
-    double bigzraw;         //The zraw in the xyz
-    double r;               //The r in the rgb
-    double g;               //The g in the rgb
-    double b;               //The b in the rgb
-    double u;               //The u' in the u'v'y
-    double v;               //The v' in the u'v'y
-    double nm;              //The nm in the nmdu'v'Y
-    double du;              //The duv' in the nmdu'v'Y
-    //    double L;               //The L in L*A*B*
-    //    double A;               //The A in L*A*B*
-    //    double B;               //The B in L*A*B*
-    NSString *redrange;		//The range which the KClmtr is in red
-    NSString *greenrange;		//The range which the KClmtr is in green
-    NSString *bluerange;		//The range which the KClmtr is in blue
-    int range;              //The range which the KClmtr is in overall
-    double temp;            //The temperature in K
-    double duv;             //The distance off the curve
-    int errorcode;          //The error code whenever you are getting data
-    NSString *errorstring;	//The string
-    int averagingby;        //How many measurements we are averaging by
-    bool readyflag;         //Ready or not to grab it
-}
--(id)initWithMeasurement:(Measurement) measure;
-@end
-@interface NSwrgb : NSObject
-{
-@public
-    //white, red, green, blue
-    //x, y, z
-    double v[4][3];
-}
--(id)initWithWRGB:(wrgb)WRGB;
--(wrgb)getNwrgb;
-@end
-
-@interface NSBlackMatrix : NSObject
-{
-@public
-    double range1[3];
-    double range2[3];
-    double range3[3];
-    double range4[3];
-    double range5[3];
-    double range6[3];
-    double Therm;
-    int errorcode;
-    NSString *errorstring;
-}
-    
--(id)initWithBlackMatrix:(BlackMatrix)black;
-@end
-
-@interface NSCorrectedCoefficient : NSObject  
-{
-@public
-    double colorMatrix[3][3];
-    double rgbMatrix[3][3];
-}
--(id)initWithCorrectedCoefficient:(CorrectedCoefficient)corrected;
-
--(CorrectedCoefficient)getNCorrectedCoefficient;
-
-@end
-
-@interface NSFlicker : NSObject
-{
-@public
-    NSMeasurement *xyz;
-    double peakfrequency[3][3];
-    double flickerDB[101];
-    double flickerPercent[101];
-    int errorcode;
-    NSString* errorstring;
-    bool readyflag;
-}
--(id)initWithFlicker:(Flicker)flicker;
-@end
-
-@interface NSColors : NSObject {
-@public
-    int red;
-    int green;
-    int blue;
-}
--(id)initwithColors:(Colors)colors;
--(Colors)getNColor;
-@end
-
-@interface  NSWhiteSpect :  NSControl  {
 @public
     double x;
     double y;
-    double z;
-    double xy;
-    double l;
+    double bigx;
+    double bigy;
+    double bigz;
+    double bigxraw;
+    double bigyraw;
+    double bigzraw;
+    double red;
+    double green;
+    double blue;
+    double u;
+    double v;
+    double nm;
+    double nmduv;
+    double L;
+    double a;
+    double b;
+    double C;
+    double h;
+    Measurement::MeasurmentRange redrange;
+    Measurement::MeasurmentRange greenrange;
+    Measurement::MeasurmentRange bluerange;
+    double temp;
+    double tempduv;
+    int errorcode;
+    int averagingby;
 }
--(id)initWithWhite:(whitespect)White;
--(whitespect)getNwhitespect;
+    
+-(id)initWithMeasurement:(Measurement) measure;   
+-(id)initwithXYZ:(double)_X Y:(double)_Y Z:(double)_Z;
+-(id)initWithXYZ:(double)_X Y:(double)_Y Z:(double)_Z gs:(gamutSpec)gs;
+-(id)initwithxyY:(double)_x y:(double)_y Y:(double)_Y;
+-(id)initwithxyY:(double)_x y:(double)_y Y:(double)_Y gs:(gamutSpec)gs;
+-(id)initwithuvprimeY:(double)_u u:(double)_v Y:(double)_Y;
+-(id)initwithuvprimeY:(double)_u u:(double)_v Y:(double)_Y gs:(gamutSpec)gs;
+-(id)initwithTempduvY:(double)_temp duv:(double)_duv Y:(double)_Y;
+-(id)initWithTempduvY:(double)_temp duv:(double)_duv Y:(double)_Y gs:(gamutSpec)gs;
+-(id)initWithRGB:(double)_red green:(double)_green blue:(double)_blue;
+-(id)initWithRGB:(double)_red green:(double)_green blue:(double)_blue gs:(gamutSpec)gs;
+
+
+-(void)convertToC:(Measurement) measure;
+
 @end
-@interface NSK10V : NSObject {
-@private
-    K10V* _k10v;
+
+@interface NSFlicker : NSObject {
+@public
+    NSMeasurement *xyz;
+    double peakfrequency[3][3];
+    double flickerDB[129];
+    double flickerPercent[129];
+    int errorcode;
 }
--(id)initWithK10V:(K10V)k10V;
--(K10V)getNK10V;
--(NSColors*)getColors;
--(void)setColors:(NSColors*)colors;
+
+-(id)initWithFlicker:(Flicker) flicker;
+
+-(void)convertToC:(Flicker) flicker;
+    
 @end
 /** @ingroup wrappers
  * 	@brief Wraps the Native object to work easly in XCode
@@ -157,7 +95,7 @@ public:
 //Properties
 -(NSString*)getPort;
 -(void)setPort:(NSString*)PortName;
--(NSString*)getSN;
+-(NSString*)getSerialNumber;
 -(NSString*)getModel;
 -(bool)isPortOpen;
 
@@ -165,20 +103,16 @@ public:
 
 //Properties - CalFiles
 -(NSString*)getCalfileName;
--(void)setCalFileName:(NSString*)calFileName;
 -(int)getCalFileID;
 -(void)setCalFileID:(int)calFileID;
--(NSMatrix*)getCalMatrix;
--(NSMatrix*)getRGBMatrix;
--(NSWhiteSpect*) getWhiteSpect;
--(void)resetWhiteSpect;
+-(matrix)getCalMatrix;
+-(matrix)getRGBMatrix;
+-(WhiteSpec)getWhiteSpec;
+-(void)resetWhiteSpec;
 
--(void)setWhiteSpect:(NSWhiteSpect*)whiteSpect;
+-(void)setWhiteSpec:(WhiteSpec)whiteSpec;
 -(NSArray*)getCalfileList;
--(NSK10V*)getK10V;
--(void)setK10V:(NSK10V*)value;
-
--(void)setTempCalFile:(NSCorrectedCoefficient*)matrix whitespect:(NSWhiteSpect*)whitespect;
+-(void)setTempCalFile:(CorrectedCoefficient)matrix whitespec:(WhiteSpec)whitespec;
 
 //Properties - FFT
 -(bool)getFFT_Cosine;
@@ -194,29 +128,29 @@ public:
 -(bool)isMeasuring;
 -(void)startMeasuring;
 -(void)stopMeasuring;
--(NSMeasurement*)getNextMeasurment;
--(NSCorrectedCoefficient*)getCofficintTestMatrix:(NSwrgb*) Reference kclmtr:(NSwrgb*)kclmtr;
+-(NSMeasurement*)getNextMeasurment:(int)n;
+-(CorrectedCoefficient)getCofficintTestMatrix:(wrgb) Reference kclmtr:(wrgb)kclmtr;
 -(int)deleteCalFile:(int)calFileID;
--(int)storeCalFile:(int)idNumber name:(NSString*)Name ref:(NSwrgb*)Reference kclmtr:(NSwrgb*)kclmtr whitespect:(NSWhiteSpect*)whitespect;
+-(int)storeCalFile:(int)idNumber name:(NSString*)Name ref:(wrgb)Reference kclmtr:(wrgb)kclmtr whitespec:(WhiteSpec)whitespec;
 
 //BlackCal - Cold
--(NSBlackMatrix*)captureBlackLevel;
--(NSBlackMatrix*)recallFlashMatrix;
+-(BlackMatrix)captureBlackLevel;
+-(BlackMatrix)getFlashMatrix;
 
 //BlackCal - hot
--(NSBlackMatrix*)recallRAMMatrix;
--(NSBlackMatrix*)recallCoefficientMatrix;
+-(BlackMatrix)getRAMMatrix;
+-(BlackMatrix)getCoefficientMatrix;
 
 //FFT
 -(bool)isFlickering;
--(NSString*)startFlicker:(bool)grabConstantly;
+-(int)startFlicker:(bool)grabConstantly;
 -(NSFlicker*)getNextFlicker;
 -(void)stopFlickering;
 
 //Setup/closing
 -(bool)connect;
 -(bool)connect:(NSString*)portName;
--(void)closePort:(bool)resetThePortName;
+-(void)closePort;
  
 /** @brief Sends out flicker
  *  @details You must setup the target where the flicker needs to go to.
