1feb0fb22d61df670d8c4261bb1382fd6a08e2bd
ThomasNelson <tom@kleininstruments.com>
2013-06-18 10:53:49 -0700

    Fixed Flicker Removed NSFlicker Removed NSMeasure Changed addTarget to addTargetForMeasure and addTargetForFlicker

--- XCode/NSKClmtr/NSKClmtr/NSKClmtr.h
+++ XCode/NSKClmtr/NSKClmtr/NSKClmtr.h
@@ -15,71 +15,10 @@ class SubClass : public KClmtr {
 public:
     SubClass(NSKClmtr* _NSK);
     void printMeasure(Measurement m);
-    void printFlick(Flicker f);
+    void printFlicker(Flicker f);
     NSKClmtr* _NSKClmtr;
 };
-@interface NSMeasurement : NSObject {
-@public
-    double x;
-    double y;
-    double bigx;
-    double bigy;
-    double bigz;
-    double bigxraw;
-    double bigyraw;
-    double bigzraw;
-    double red;
-    double green;
-    double blue;
-    double u;
-    double v;
-    double nm;
-    double nmduv;
-    double L;
-    double a;
-    double b;
-    double C;
-    double h;
-    Measurement::MeasurmentRange redrange;
-    Measurement::MeasurmentRange greenrange;
-    Measurement::MeasurmentRange bluerange;
-    double temp;
-    double tempduv;
-    int errorcode;
-    int averagingby;
-}
-    
--(id)initWithMeasurement:(Measurement) measure;   
--(id)initwithXYZ:(double)_X Y:(double)_Y Z:(double)_Z;
--(id)initWithXYZ:(double)_X Y:(double)_Y Z:(double)_Z gs:(gamutSpec)gs;
--(id)initwithxyY:(double)_x y:(double)_y Y:(double)_Y;
--(id)initwithxyY:(double)_x y:(double)_y Y:(double)_Y gs:(gamutSpec)gs;
--(id)initwithuvprimeY:(double)_u u:(double)_v Y:(double)_Y;
--(id)initwithuvprimeY:(double)_u u:(double)_v Y:(double)_Y gs:(gamutSpec)gs;
--(id)initwithTempduvY:(double)_temp duv:(double)_duv Y:(double)_Y;
--(id)initWithTempduvY:(double)_temp duv:(double)_duv Y:(double)_Y gs:(gamutSpec)gs;
--(id)initWithRGB:(double)_red green:(double)_green blue:(double)_blue;
--(id)initWithRGB:(double)_red green:(double)_green blue:(double)_blue gs:(gamutSpec)gs;
 
-
--(void)convertToC:(Measurement) measure;
-
-@end
-
-@interface NSFlicker : NSObject {
-@public
-    NSMeasurement *xyz;
-    double peakfrequency[3][3];
-    double flickerDB[129];
-    double flickerPercent[129];
-    int errorcode;
-}
-
--(id)initWithFlicker:(Flicker) flicker;
-
--(void)convertToC:(Flicker) flicker;
-    
-@end
 /** @ingroup wrappers
  * 	@brief Wraps the Native object to work easly in XCode
  */
@@ -87,7 +26,8 @@ public:
 
 @private
     SubClass* _kclmtr;
-    id _target;
+    id targetMeasure;
+    id targetFlicker;
     SEL printMeasure;
     SEL printFlicker;
 }
@@ -128,7 +68,7 @@ public:
 -(bool)isMeasuring;
 -(void)startMeasuring;
 -(void)stopMeasuring;
--(NSMeasurement*)getNextMeasurment:(int)n;
+-(AvgMeasurement)getNextMeasurment:(int)n;
 -(CorrectedCoefficient)getCofficintTestMatrix:(wrgb) Reference kclmtr:(wrgb)kclmtr;
 -(int)deleteCalFile:(int)calFileID;
 -(int)storeCalFile:(int)idNumber name:(NSString*)Name ref:(wrgb)Reference kclmtr:(wrgb)kclmtr whitespec:(WhiteSpec)whitespec;
@@ -144,7 +84,7 @@ public:
 //FFT
 -(bool)isFlickering;
 -(int)startFlicker:(bool)grabConstantly;
--(NSFlicker*)getNextFlicker;
+-(Flicker)getNextFlicker;
 -(void)stopFlickering;
 
 //Setup/closing
@@ -160,7 +100,7 @@ public:
  *   Source
  *  @snippet NSKClmtrExample.mm Source_flicker
  */
--(void)sendFlicker:(NSFlicker*)flicker;
+-(void)sendFlicker:(Flicker)flicker;
 /** @brief Sends out measurement
  *  @details You must setup the target where the measure needs to go to.
  *  @details Here is an example: 
@@ -169,12 +109,16 @@ public:
  *   Source
  *  @snippet NSKClmtrExample.mm Source_measure
  */
--(void)sendMeasure:(NSMeasurement*)measurement;
-/** @brief Sets up where the measurement/flicker goes to
+-(void)sendMeasure:(Measurement)measurement;
+/** @brief Sets up where the measurement goes to
  *  @param target The object that needs to be sent to
  *  @param action The function that needs to be sent to
- *  @param measure If it's for measruement = TRUE, if it's for flicker = False
  */
--(void)addTarget:(id)target action:(SEL)action forEvent:(bool)measure;
+-(void)addTargetForMeasure:(id)target action:(SEL)action;
+/** @brief Sets up where the flicker goes to
+ *  @param target The object that needs to be sent to
+ *  @param action The function that needs to be sent to
+ */
+-(void)addTargetForFlicker:(id)target action:(SEL)action;
 
 @end
--- XCode/NSKClmtr/NSKClmtr/NSKClmtr.mm
+++ XCode/NSKClmtr/NSKClmtr/NSKClmtr.mm
@@ -12,12 +12,11 @@ SubClass::SubClass(NSKClmtr* _NSK)
 {
     _NSKClmtr = _NSK;
 }
-void SubClass::printFlick(Flicker f)
+void SubClass::printFlicker(Flicker f)
 {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
     
-    NSFlicker *flicker = [[NSFlicker alloc] initWithFlicker:f];
-    [_NSKClmtr sendFlicker:flicker];
+    [_NSKClmtr sendFlicker:f];
     
     [pool release];
 }
@@ -25,160 +24,11 @@ void SubClass::printMeasure(Measurement m)
 {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
     
-    NSMeasurement *measure = [[NSMeasurement alloc] initWithMeasurement:m];
-    [_NSKClmtr sendMeasure:measure];
+    [_NSKClmtr sendMeasure:m];
 
     [pool release];
 
 }
-@implementation NSMeasurement
-
--(id)init{
-    self = [super init];
-    if(self){
-        [self convertToC:Measurement()];
-    }
-    return self;
-}
-
--(id)initWithMeasurement:(Measurement)measure{
-    self = [super init];
-    if(self){
-        [self convertToC:Measurement()];
-    }
-    return self;
-}
--(id)initwithXYZ:(double)_X Y:(double)_Y Z:(double)_Z{
-    self = [super init];
-    if(self){
-        [self convertToC:Measurement::fromXYZ(_X, _Y, _Z)];
-    }
-    return self;
-}
--(id)initWithXYZ:(double)_X Y:(double)_Y Z:(double)_Z gs:(gamutSpec)gs{
-    self = [super init];
-    if(self){
-        [self convertToC:Measurement::fromXYZ(_X, _Y, _Z, gs)];
-    }
-    return self;
-}
--(id)initwithxyY:(double)_x y:(double)_y Y:(double)_Y{
-    self = [super init];
-    if(self){
-        [self convertToC:Measurement::fromxyY(_x, _y, _Y)];
-    }
-    return self;
-}
--(id)initwithxyY:(double)_x y:(double)_y Y:(double)_Y gs:(gamutSpec)gs{
-    self = [super init];
-    if(self){
-        [self convertToC:Measurement::fromxyY(_x, _y, _Y, gs)];
-    }
-    return self;
-}
--(id)initwithuvprimeY:(double)_u u:(double)_v Y:(double)_Y{
-    self = [super init];
-    if(self){
-        [self convertToC:Measurement::fromuvprimeY(_u, _v, _Y)];
-    }
-    return self;
-}
--(id)initwithuvprimeY:(double)_u u:(double)_v Y:(double)_Y gs:(gamutSpec)gs{
-    self = [super init];
-    if(self){
-        [self convertToC:Measurement::fromuvprimeY(_u, _v, _Y, gs)];
-    }
-    return self;
-}
--(id)initwithTempduvY:(double)_temp duv:(double)_duv Y:(double)_Y{
-    self = [super init];
-    if(self){
-        [self convertToC:Measurement::fromTempduvY(_temp, _duv, _Y)];
-    }
-    return self;
-}
--(id)initWithTempduvY:(double)_temp duv:(double)_duv Y:(double)_Y gs:(gamutSpec)gs{
-    self = [super init];
-    if(self){
-        [self convertToC:Measurement::fromTempduvY(_temp, _duv, _Y, gs)];
-    }
-    return self;
-}
--(id)initWithRGB:(double)_red green:(double)_green blue:(double)_blue{
-    self = [super init];
-    if(self){
-        [self convertToC:Measurement::fromRGB(_red, _green, _blue)];
-    }
-    return self;
-}
--(id)initWithRGB:(double)_red green:(double)_green blue:(double)_blue gs:(gamutSpec)gs{
-    self = [super init];
-    if(self){
-        [self convertToC:Measurement::fromRGB(_red, _green, _blue, gs)];
-    }
-    return self;
-}
--(void)convertToC:(Measurement)measure{
-    x = measure.x;
-    y = measure.y;
-    bigx = measure.bigx;
-    bigy = measure.bigy;
-    bigz = measure.bigz;
-    bigxraw = measure.bigxraw;
-    bigyraw = measure.bigyraw;
-    bigzraw = measure.bigzraw;
-    red = measure.red;
-    green = measure.green;
-    blue = measure.blue;
-    u = measure.u;
-    v = measure.v;
-    nm = measure.nm;
-    nmduv = measure.nmduv;
-    L = measure.L;
-    a = measure.a;
-    b = measure.b;
-    C = measure.C;
-    h = measure.h;
-    redrange = measure.redrange;
-    greenrange = measure.greenrange;
-    bluerange = measure.bluerange;
-    temp = measure.temp;
-    tempduv = measure.tempduv;
-    errorcode = measure.errorcode;
-    averagingby = measure.averagingby;
-}
-
-@end
-
-@implementation NSFlicker
--(id)init{
-    self = [super init];
-    if(self){
-        [self convertToC:Flicker()];
-    }
-    return self;
-}
--(id)initWithFlicker:(Flicker) flicker{
-    self = [super init];
-    if(self){
-        [self convertToC:flicker];
-    }
-    return self;
-}
--(void)convertToC:(Flicker) flicker {
-    [xyz initWithMeasurement:flicker.xyz];
-    for(int i = 0; i < 3; ++i){
-        for(int x = 0; x < 3; ++x){
-            peakfrequency[i][x] = flicker.peakfrequency[i][x];
-        }
-    }
-    for(int i = 0; i < 129; ++i){
-        flickerDB[i] = flicker.flickerDB[i];
-        flickerPercent[i] = flicker.flickerPercent[i];
-    }
-    errorcode = flicker.errorcode;
-}
-@end
 
 @implementation NSKClmtr
 
@@ -294,8 +144,8 @@ void SubClass::printMeasure(Measurement m)
 -(void)stopMeasuring{
     _kclmtr->stopMeasuring();
 }
--(NSMeasurement*)getNextMeasurment:(int)n{
-    return [[NSMeasurement alloc] initWithMeasurement:_kclmtr->getNextMeasurement(n)];
+-(AvgMeasurement)getNextMeasurment:(int)n{
+    return _kclmtr->getNextMeasurement(n);
 }
 -(CorrectedCoefficient)getCofficintTestMatrix:(wrgb)Reference kclmtr:(wrgb)kclmtr{
     return _kclmtr->getCoefficientTestMatrix(Reference, kclmtr);
@@ -330,8 +180,8 @@ void SubClass::printMeasure(Measurement m)
 -(int)startFlicker:(bool)grabConstantly{
     return _kclmtr->startFlicker(grabConstantly);
 }
--(NSFlicker*)getNextFlicker{
-    return [[NSFlicker alloc] initWithFlicker:_kclmtr->getNextFlicker()];
+-(Flicker)getNextFlicker{
+    return _kclmtr->getNextFlicker();
 }
 -(void)stopFlickering{
     _kclmtr->stopFlicker();
@@ -349,21 +199,23 @@ void SubClass::printMeasure(Measurement m)
     _kclmtr->closePort();
 }
 
--(void)sendMeasure:(NSMeasurement*)measurement
+-(void)sendMeasure:(Measurement)measurement
 {
-    [_target performSelector:printMeasure withObject:measurement];
+    [targetMeasure performSelector:printMeasure withObject:[NSValue value:&measurement withObjCType:@encode(Measurement)]] ;
 }
--(void)sendFlicker:(NSFlicker*)flicker
+-(void)sendFlicker:(Flicker)flicker
 {
-    [_target performSelector:printFlicker withObject:flicker];
+    [targetFlicker performSelector:printFlicker withObject:[NSValue value:&flicker withObjCType:@encode(Flicker)]];
 }
--(void)addTarget:(id)target action:(SEL)action forEvent:(bool)measure
+-(void)addTargetForMeasure:(id)target action:(SEL)action
 {
-    self->_target = target;
-    if(measure)
-        printMeasure = action;
-    else
-        printFlicker = action;
+    targetMeasure = target;
+    printMeasure = action;
+}
+-(void)addTargetForFlicker:(id)target action:(SEL)action
+{
+    targetFlicker = target;
+    printFlicker = action;
 }
 
 @end
